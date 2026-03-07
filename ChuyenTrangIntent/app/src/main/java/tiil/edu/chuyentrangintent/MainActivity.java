6fefb28f67a44eae9b0e1345fa9e08a757e07c52
hung090725 <nguyenhung27071999@gmail.com>
2026-03-07 21:38:39 +0700

    chuyển dữ liệu

--- ChuyenTrangIntent/app/src/main/java/tiil/edu/chuyentrangintent/MainActivity.java
+++ ChuyenTrangIntent/app/src/main/java/tiil/edu/chuyentrangintent/MainActivity.java
@@ -1,9 +1,9 @@
 package tiil.edu.chuyentrangintent;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
         // tham số 1 là hiện tại 2 là gửi tới(.class)
         Intent ThuKichHoatMH2 = new Intent(MainActivity.this, // MH hiện  tại
                                                     MainActivity2.class); // màng hình chuyển tới
+
+       // 1+ gói dữ liệu sẽ có các bước con bên trong
+        // tìm điều khiển chứa dữ liệu
+        EditText  edtHoTen = findViewById(R.id.edtHoVaTenMH1);
+        EditText  edtTuoi = findViewById(R.id.edtNumber);
+        //1.2 lấy dữ liệu ra từ các điều khiển tương ứng
+        String HoTen = edtHoTen.getText().toString();
+        String Tuoi = edtTuoi.getText().toString();
+        // 1.3 Gói vào thư mỗi dữ liệu ta đặt  cho một cái key/ name để bên kia bóc ra
+        // putExtra thêm thu vào tham số 1 từ  khóa để liên kết bên kia , tham số 2 là giá trị đc lưu trữ nội dung thu
+
+        ThuKichHoatMH2.putExtra("tete",HoTen);
+        ThuKichHoatMH2.putExtra("tuoi",Tuoi);
+
+
+
         // 2.Gửi Thư , mà không đợi phản hồi
         startActivity(ThuKichHoatMH2);
     }
--- ChuyenTrangIntent/app/src/main/java/tiil/edu/chuyentrangintent/MainActivity2.java
+++ ChuyenTrangIntent/app/src/main/java/tiil/edu/chuyentrangintent/MainActivity2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,15 +19,36 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         // tìm đối tượng gắn bộ lắng nghe lưu trữ nó ở Button vì tìm ở button
         Button BtnMH2 = findViewById(R.id.btnmh2);
         // gắn bộ lắng nghe
         BtnMH2.setOnClickListener(BoLangNGheChuyenMH1);
+
+        //khi màng hình mở ra ta lấy Intent đã kích hoạt dùng getIntent tự hiểu lấy thư nào luôn
+        Intent  ThuNhanDuoc = getIntent();
+        // bóc dữ liệu ra
+         String layraten = ThuNhanDuoc.getStringExtra("tete");
+         String laytuoi = ThuNhanDuoc.getStringExtra("tuoi");
+         // bóc xong phải đặt lên vị trí nó
+         // tìm điều khiển chứa dữ liệu
+        TextView  TVTen = findViewById(R.id.TVHoTen);
+        TextView  TVTuoi = findViewById(R.id.TVTuoiMH2);
+        //1.2 lấy dữ liệu ra từ các điều khiển tương ứng
+        /*
+         TVTen: Là cái "bảng" (đối tượng TextView) mà bạn đã tìm thấy ở dòng trên bằng findViewById.
+         setText: Thực hiện việc ghi thông tin đó lên màn hình.
+         . (dấu chấm): Nghĩa là bạn đang ra lệnh cho cái "bảng" đó làm một việc gì đó.
+         setText(...): Là hành động bạn yêu cầu cái bảng thực hiện (hãy hiển thị chữ đi).
+layraten: Là "nội dung" bạn muốn ghi lên. Ở đây là cái tên mà bạn vừa "bóc" ra từ lá thư Intent.*/
+        TVTen.setText(layraten);
+        TVTuoi.setText("Tuổi: "+ laytuoi);
+
 
     }
 
